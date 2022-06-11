1a1b5dfab4101593def3069daab409f7166d4164
DEV-BOOK\tiago <tiagornandrade@hotmail.com>
2022-06-11 16:29:39 -0300

    feat: criacao etl Iris

--- analytic_iris/jobs/job_iris.py
+++ analytic_iris/jobs/job_iris.py
@@ -1,7 +1,26 @@
 from dagster import job
 
-from analytic_iris.ops.iris import etl_iris
+from analytic_iris.ops.iris import (
+    etl_iris,
+    array_data,
+    array_labels,
+    getting_label_names,
+    fitting_predictions,
+    splitting_train_test,
+    fit_data,
+    predict_data,
+    save_result,
+)
+
 
 @job
 def etl():
     pre_load = etl_iris()
+    array_x = array_data(pre_load)
+    array_y = array_labels(pre_load)
+    getting_label_names(pre_load)
+    fit_pred = fitting_predictions(array_x)
+    splitting_train_test(array_x, array_y, fit_pred)
+    fit = fit_data(array_x, array_y, fit_pred)
+    predict = predict_data(fit, array_x)
+    save_result(predict)
--- analytic_iris/ops/iris.py
+++ analytic_iris/ops/iris.py
@@ -1,13 +1,70 @@
-import os
-import pandas as pd
+from sklearn.datasets import load_iris
+from sklearn import tree
+from sklearn.metrics import accuracy_score
+import numpy as np
+import csv
 from dagster import op
 
 
 @op
 def etl_iris(context):
-    data = 'https://raw.githubusercontent.com/plotly/datasets/master/iris-data.csv'
-    data_path = "c:\\Temp\\stage\\"
+    iris = load_iris()
+    return iris
 
-    iris = pd.read_csv(data)
-    iris = iris[(iris["class"] == 'Iris-setosa') & (iris["sepal length"] > 5.0)]
-    iris.to_csv(data_path + "iris_filtro.csv", index=False)
+
+@op
+def array_data(context, iris):
+    x = iris.data  # array of the data
+    return x
+
+
+@op
+def array_labels(context, iris):
+    y = iris.target  # array of labels (i.e answers) of each data entry
+    return y
+
+
+@op
+def getting_label_names(context, iris):
+    y_names = iris.target_names
+    return y_names
+
+
+@op
+def fitting_predictions(context, x):
+    test_ids = np.random.permutation(len(x))
+    return test_ids
+
+
+@op
+def splitting_train_test(context, x, y, test_ids):
+    x_train = x[test_ids[:-10]]
+    y_train = y[test_ids[:-10]]
+    x_test = x[test_ids[-10:]]
+    y_test = y[test_ids[-10:]]
+    return (
+        x_train,
+        y_train,
+        x_test,
+        y_test,
+    )
+
+
+@op
+def fit_data(context, x, y, test_ids):
+    clf = tree.DecisionTreeClassifier()
+    clf.fit(x[test_ids[:-10]], y[test_ids[:-10]])
+    return clf
+
+
+@op
+def predict_data(context, clf, x_test):
+    pred = clf.predict(x_test)
+    return pred
+
+
+@op
+def save_result(context, pred):
+    with open("prediciton.csv", "w", newline="") as csvfile:
+        writer = csv.writer(csvfile, delimiter=" ")
+        writer.writerow(pred)
